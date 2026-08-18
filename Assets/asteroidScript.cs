5c374b0326901e5b2041dfab9395f8fcadd0c6eb
rocharafael8011-lang <rocharafael8011@gmail.com>
2026-08-18 09:09:35 -0300

    Add stage manager and update asteroid logic
    Introduce stageManager.cs and stageManagerScript.cs to initialize players, stage level, scores and lives, and to expose UI Text fields. Update asteroidScript.cs: add a position field, refine OnTriggerEnter2D to respond to "tiro1" collisions, spawn smaller asteroid fragments at offsets, instantiate explosion, award ScorePlayer2 points based on asteroid size, and destroy the original. Also includes Unity .meta and Library metadata updates.

--- Assets/asteroidScript.cs
+++ Assets/asteroidScript.cs
@@ -8,9 +8,9 @@ public class asteroidScript : MonoBehaviour {
 	public GameObject asteroid1, asteroid2, asteroid3, explosion;
 	public float speed; 
 	public Rigidbody2D rb;
-
-		
+	public Vector3 pos;
 	
+
 	void start () {
 		switch (tamanho)
 		{
@@ -38,28 +38,33 @@ public class asteroidScript : MonoBehaviour {
 
 	void OnTriggerEnter2D ( Collider2D collider ) {
 		
+		if(collider.gameObject.tag == "tiro1") {
 
-		if(collider.gameObject.tag == "tiro1" || collider.gameObject.tag == "tiro2"){
 		switch (tamanho)
-		{
-			case 3:
-			Instantiate(explosion, this.transform.position,this.transform.rotation);
-			Instantiate(asteroid2, transform.position,Quaternion.identity);
-			Instantiate(asteroid2, transform.position,Quaternion.identity);
-			Destroy(this.gameObject,0f);
-			break;
+				{
+				case 3:
+				Instantiate(explosion, this.transform.position,this.transform.rotation);
+				pos = transform.position;
+				Instantiate(asteroid2, pos = new Vector3(-0.5f,0.5f, 0f), Quaternion.identity);
+				Instantiate(asteroid2, pos = new Vector3(0.5f,-0.5, 0f), Quaternion.identity);
+				stageManagerScript.ScorePlayer2+= 100f;
+				Destroy(this.gameObject,0f);
+				break;
 
-			case 2:
-			Instantiate(explosion, this.transform.position,this.transform.rotation);
-			Instantiate(asteroid1, transform.position,Quaternion.identity);
-			Instantiate(asteroid1, transform.position,Quaternion.identity);
-			Destroy(this.gameObject,0f);
-			break;
+				case 2:
+				Instantiate(explosion, this.transform.position,this.transform.rotation);
+				pos = transform.position;
+				Instantiate(asteroid1, pos + new  Vector3(-0.5f,0.5f, 0f), Quaternion.identity);
+				Instantiate(asteroid1, pos + new  Vector3(0.5f,-0.5f, 0f),Quaternion.identity);
+				stageManagerScript.ScorePlayer2+= 50f;
+				Destroy(this.gameObject,0f);
+				break;
 
-			case 1:
-			Instantiate(explosion, this.transform.position, this.transform.rotation);
-			Destroy(this.gameObject,0f);
-			break;
+				case 1:
+				Instantiate(explosion, this.transform.position, this.transform.rotation);
+				stageManagerScript.ScorePlayer2+= 15f;
+				Destroy(this.gameObject,0f);
+				break;
 
 		}
 	
---
+++ Assets/stageManagerScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class stageManagerScript : MonoBehaviour {
+
+		public GameObject PlayerOne,PlayerTwo,Asteroid1,Asteroid2,Asteroid3;
+		public int numPlayer;
+		public GameObject atualPlayer1,atualPlayer2;
+		public int stageLevel;
+		public static float ScorePlayer1,ScorePlayer2; //vidas do player
+		public static int player1Life,player2Life; //vidas do player
+	
+	public Text TXT_PontosP1,TXT_PontosP2;
+	public Text TXT_LIFEP1,TXT_LIFEP2;
+
+	void Start () {
+
+			IniciarLevel();
+		
+	}
+	void Update () {
+	//===========================Player1==================================
+	TXT_PontosP1.text = "Pontos P1: " +((int)ScorePlayer1).ToString("D6");
+	TXT_LIFEP1.text = "Vidas = " + ((int)player1Life).ToString("D2");
+	//===========================Player2==================================
+	TXT_PontosP2.text = "PontosP2: " + ((int)ScorePlayer2).ToString("D6");
+	TXT_LIFEP2.text = "Vidas = " + ((int)player2Life).ToString("D2");
+
+		
+	}
+
+	void IniciarLevel(){
+
+	if(numPlayer==2){
+			atualPlayer1 = Instantiate(PlayerOne, transform.position,Quaternion.identity);
+			atualPlayer2 = Instantiate(PlayerTwo, transform.position,Quaternion.identity);
+	}else{
+			atualPlayer1 = Instantiate(PlayerOne, transform.position,Quaternion.identity);
+	}
+	stageLevel = 1;
+	ScorePlayer1 = 0f;
+	ScorePlayer2 = 0f;
+	player1Life = 3;
+	player2Life = 3;
+	}
+
+}
